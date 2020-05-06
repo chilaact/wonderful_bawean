ebcb39bc789028c1169b34a83fabe928c2e1ae1c
GunAP <gunturadhip06@gmail.com>
2020-05-07 00:04:30 +0700

    Auto stash before merge of "master" and "origin/master"

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -37,5 +37,28 @@
               <div class="text-center">
                 <a class="small" href="<?=site_url('C_login/index') ?>">Already have an account? Login!</a>
               </div>
+<<<<<<< HEAD
+=======
+				<!-- Login Form -->
+				<form action="<?php echo base_url('C_registration/register'); ?>" method="post">
+				<input type="text" id="username" class="fadeIn second" name="Nama Lengkap" placeholder="nama lengkap" required>
+				<input type="text" id="password" class="fadeIn third" name="Email" placeholder="email" required>
+				<input type="text" id="password" class="fadeIn third" name="Telepon" placeholder="telepon" required>
+				<input type="text" id="password" class="fadeIn third" name="Alamat" placeholder="alamat" required>
+				<input type="text" id="password" class="fadeIn third" name="Username" placeholder="username" required>
+				<input type="text" id="password" class="fadeIn third" name="Password" placeholder="password" required>
+				<input type="text" id="password" class="fadeIn third" name="Confirm Password" placeholder="confirm password" required>
+
+				<input type="submit" class="fadeIn fourth" value="Register">
+				</form>
+
+				<!-- Remind Passowrd -->
+				<div id="formFooter">
+				<a class="underlineHover" href="<?= site_url('C_login/index'); ?>">Log In</a><br>
+				</div>
+		</div>
+>>>>>>> master
+=======
+>>>>>>> d24d7756c1d71ccb4b29529ef6039616c5b987f3
 </body>
 </html>
