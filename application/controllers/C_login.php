d186ec847161ae1dab0839adb45acdc653a7e2da
Fik <fikrirbb@gmail.com>
2020-05-06 23:24:12 +0700

    [#] fixing login, register

--- application/controllers/C_login.php
+++ application/controllers/C_login.php
@@ -14,42 +14,41 @@ class C_login extends CI_Controller {
 	}
 
 	public function registration(){
-    $this->load->library('form_validation');
-    if ($this->form_validation->run() == false){
-      
-      $this->load->view('auth/registration');
-    }
-    else {
-      $data = [
-        'us_nama'=> $this->input->post('username'),
-        'us_email'=> $this->input->post('email'),
-        'us_nohp'=> $this->input->post('no_hp'),
-        'us_image'=> 'user.jpg',//grong enek gambar e
-        'us_alamat'=> $this->input->post('alamat'),
-        'us_password'=> $this->input->post('password'),
-        'us_role'=> 0,//iki rek digawe piro default e
-        'us_date_crated'=> time(),
-        'us_date_update'=> time(),
-        'us_status'=> 0,
-      ];
-      $this->db->insert('user',$data);
-    }
-   
-	}
+    $this->load->view('auth/registration');   
+  }
+  
+  function aksiRegister()
+  {
+    $data = [
+      'us_nama'=> $this->input->post('username'),
+      'us_email'=> $this->input->post('email'),
+      'us_nohp'=> $this->input->post('no_hp'),
+      'us_img'=> 'user.jpg',//grong enek gambar e
+      'us_alamat'=> $this->input->post('alamat'),
+      'us_password'=> md5($this->input->post('password')),
+      'us_role'=> 0,//iki rek digawe piro default e
+      'us_date_created'=> time(),
+      'us_date_update'=> time(),
+      'us_status'=> 0,
+    ];
+    $this->db->insert('user',$data);
+    redirect('c_login');
+    
+  }
 
 	function aksi_login(){
 		
-	$username = $this->input->post('username'); // Ambil isi dari inputan username pada form login
+	$username = $this->input->post('email'); // Ambil isi dari inputan username pada form login
     $password = $this->input->post('password'); // Ambil isi dari inputan password pada form login dan encrypt dengan md5
     $user = $this->m_login->get($username); // Panggil fungsi get yang ada di UserModel.php
 
 
     if(empty($user)){ // Jika hasilnya kosong / user tidak ditemukan
       $this->session->set_flashdata('message', 'Username tidak ditemukan'); // Buat session flashdata
-      echo "gk ada";
+      // echo "gk ada";
       // redirect('auth/login'); // Redirect ke halaman login
     }else{
-      if($password == $user->us_password){ // Jika password yang diinput sama dengan password yang didatabase
+      if(md5($password) == $user->us_password){ // Jika password yang diinput sama dengan password yang didatabase
         $session = array(
           'authenticated'=>true, // Buat session authenticated dengan value true
           'username'=>$user->username,  // Buat session username
--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -20,17 +20,15 @@
 				<div class="fadeIn first">
 				<img src="<?php echo base_url() ?>assets/image/wb.png" id="icon" alt="User Icon" />
 				</div>
-
-<<<<<<< HEAD
 		    <!-- Login Form -->
-			<form class="user" method="post" action="<?php echo base_url('C_login/registration'); ?>">
+			<form class="user" method="post" action="<?php echo base_url('C_login/aksiRegister'); ?>">
                 
                 <div class="form-group">
                   <input type="text" id="username" class="fadeIn second" name="username" placeholder="username" required>
                   <input type="text" id="email" class="fadeIn second" name="email" placeholder="Email" required>
                   <input type="text" id="no_hp" class="fadeIn second" name="no_hp" placeholder="No Telepon" required>
                   <input type="text" id="alamat" class="fadeIn second" name="alamat" placeholder="Alamat" required>
-                  <input type="text" id="password" class="fadeIn second" name="password" placeholder="Password" required>
+                  <input type="password" id="password" class="fadeIn second" name="password" placeholder="Password" required>
                 </div>
                 <button type="submit" class="btn btn-primary btn-user btn-block">
                   Register Account
@@ -39,25 +37,5 @@
               <div class="text-center">
                 <a class="small" href="<?=site_url('C_login/index') ?>">Already have an account? Login!</a>
               </div>
-=======
-				<!-- Login Form -->
-				<form action="<?php echo base_url('C_login/aksi_login'); ?>" method="post">
-				<input type="text" id="username" class="fadeIn second" name="Nama Lengkap" placeholder="nama lengkap" required>
-				<input type="text" id="password" class="fadeIn third" name="Email" placeholder="email" required>
-				<input type="text" id="password" class="fadeIn third" name="Telepon" placeholder="telepon" required>
-				<input type="text" id="password" class="fadeIn third" name="Alamat" placeholder="alamat" required>
-				<input type="text" id="password" class="fadeIn third" name="Username" placeholder="username" required>
-				<input type="text" id="password" class="fadeIn third" name="Password" placeholder="password" required>
-				<input type="text" id="password" class="fadeIn third" name="Confirm Password" placeholder="confirm password" required>
-
-				<input type="submit" class="fadeIn fourth" value="Register">
-				</form>
-
-				<!-- Remind Passowrd -->
-				<div id="formFooter">
-				<a class="underlineHover" href="<?= site_url('C_login/index'); ?>">Log In</a><br>
-				</div>
-		</div>
->>>>>>> master
 </body>
 </html>
