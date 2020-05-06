ef65c55b549ddde1a9c6680c15c9396846582922
Chilyatun NIsa <nchilyatun@gmail.com>
2020-05-07 00:52:45 +0700

    # update login

--- application/models/M_login.php
+++ application/models/M_login.php
@@ -2,7 +2,7 @@
  
 class M_login extends CI_Model{	
 	public function get($username){
-        $this->db->where('us_nama', $username); // Untuk menambahkan Where Clause : username='$username'
+        $this->db->where('us_email', $username); // Untuk menambahkan Where Clause : username='$username'
         $result = $this->db->get('user')->row(); // Untuk mengeksekusi dan mengambil data hasil query
         return $result;
     }
