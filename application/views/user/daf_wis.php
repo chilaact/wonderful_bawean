8bc5aa0caf447f250c8abe65d4e13286097196fb
Rahayu Amelia <42790575+rahayuamelia@users.noreply.github.com>
2020-05-14 12:25:27 +0700

    Update daf_wis.php

--- application/views/user/daf_wis.php
+++ application/views/user/daf_wis.php
@@ -30,8 +30,8 @@
                   <h3><?php echo $wis->wis_nama ?></h3>
                   <p><?php echo $wis->wis_desc_short ?></p>
                   Harga Tiket Masuk &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class="badge badge-bg badge-success"> Rp. <?php echo $wis->wis_hrg_weekday ?> ,-</span><br>
-                  <a href="#" class="btn btn-sm btn-danger">Pesan</a>
-                  <?php echo anchor('C_wisata/detail_wis/'.$wis->wis_id, '<div class="btn btn-sm btn-primary">Detail</div>') ?> <!-- <a href="#" class="btn btn-sm btn-danger">Detail</a> -->
+                  <a href="#" class="badge badge-bg badge-danger" style="width: 11em; height: 2em ">Pesan</a>
+                  <?php echo anchor('C_wisata/detail_wis/'.$wis->wis_id, '<span class="badge badge-bg badge-primary" style="width: 11em; height: 2em ">Detail</span>') ?> <!-- <a href="#" class="btn btn-sm btn-danger">Detail</a> -->
                 </div>
   
               </div> 
@@ -39,73 +39,6 @@
           </div>
 
           	<?php endforeach; ?>
-
-<!--           <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
-              <div class="ftco-media-1">
-                  <div class="ftco-media-1-inner">
-                    <a href="property-single.html" class="d-inline-block mb-4"><img src="<?php echo base_url() ?>assets/image/noko.jpg" alt="Image" class="img-fluid"></a>
-                    <div class="ftco-media-details">
-                      <h3>Pulau Noko</h3>
-                      <p>Ds. Sidogedungbatu - BAWEAN</p>
-                      Harga Tiket Masuk <strong class="badge badge-bg badge-success">$20,000,000</strong>
-                    </div> 
-      
-                  </div> 
-                </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
-              <div class="ftco-media-1">
-                  <div class="ftco-media-1-inner">
-                    <a href="property-single.html" class="d-inline-block mb-4"><img src="<?php echo base_url() ?>assets/image/mangrove3.jpg" alt="Image" class="img-fluid"></a>
-                    <div class="ftco-media-details">
-                      <h3>Hutan Mangrove</h3>
-                      <p>Ds. Daun - BAWEAN</p>
-                      Harga Tiket Masuk <strong class="badge badge-bg badge-success">$20,000,000</strong>
-                    </div>
-      
-                  </div> 
-                </div>
-          </div>
-
-          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
-              <div class="ftco-media-1">
-                <div class="ftco-media-1-inner">
-                  <a href="property-single.html" class="d-inline-block mb-4"><img src="<?php echo base_url() ?>assets/image/mombhul4.jpg" alt="Image" class="img-fluid"></a>
-                  <div class="ftco-media-details">
-                    <h3>Mombhul Beach</h3>
-                    <p>Ds. Sidogedungbatu - BAWEAN</p>
-                    Harga Tiket Masuk <strong class="badge badge-bg badge-success">$20,000,000</strong>
-                  </div>
-    
-                </div> 
-              </div>
-            </div>
-            <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
-                <div class="ftco-media-1">
-                    <div class="ftco-media-1-inner">
-                      <a href="property-single.html" class="d-inline-block mb-4"><img src="<?php echo base_url() ?>assets/image/airterjun.jpg" alt="Image" class="img-fluid"></a>
-                      <div class="ftco-media-details">
-                        <h3>Laccar Waterfall</h3>
-                        <p>Dsn. Laccar - BAWEAN</p>
-                        Harga Tiket Masuk <strong class="badge badge-bg badge-success">$20,000,000</strong>
-                      </div>
-        
-                    </div> 
-                  </div>
-            </div>
-            <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
-                <div class="ftco-media-1">
-                    <div class="ftco-media-1-inner">
-                      <a href="property-single.html" class="d-inline-block mb-4"><img src="<?php echo base_url() ?>assets/image/tanjungan.jpg" alt="Image" class="img-fluid"></a>
-                      <div class="ftco-media-details">
-                        <h3>Tanjung Ghe'en</h3>
-                        <p>Ds. Komalasa - BAWEAN</p>
-                        Harga Tiket Masuk <strong class="badge badge-bg badge-success">$20,000,000</strong>
-                      </div>
-        
-                    </div> 
-                  </div>
-            </div> -->
 
         </div>
       </div>
