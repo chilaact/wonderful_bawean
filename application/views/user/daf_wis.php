39e485eadc479b8d56a356a80d6904403aa067b2
Rahayu Amelia <42790575+rahayuamelia@users.noreply.github.com>
2020-05-14 21:52:28 +0700

    update daf_wis

--- application/views/user/daf_wis.php
+++ application/views/user/daf_wis.php
@@ -1,6 +1,6 @@
-  <div class="site-block-wrap">
+  <div class="site-block-wrap" style="height: 5em">
     <div class="owl-carousel with-dots">
-      <div class="site-blocks-cover overlay overlay" style="background-color: #5F9EA0;" data-aos="fade" id="home-section">
+      <div class="site-blocks-cover overlay overlay" style="background-color: #5F9EA0; height: 5em" data-aos="fade" id="home-section">
         <div class="container">
           <div class="row align-items-center justify-content-center">
             <div class="col-md-6 mt-lg-5 text-center">
