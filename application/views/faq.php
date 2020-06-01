394958b9c33e28873994588cc487714b6bda5619
Chilyatun NIsa <nchilyatun@gmail.com>
2020-06-01 22:01:41 +0700

    [#] update faq

--- application/views/faq.php
+++ application/views/faq.php
@@ -2,35 +2,34 @@
 	<br>
 	<a href="<?php echo base_url('C_home/index'); ?>" class="btn btn-primary right">Kembali</a>
 	<div class="faq-left">
-		<h2>.:: FAQ ::.</h2>
+		<h2>FAQ | Frequently Asked Questions</h2>
 				<ul class="faq-container">
 
 						<li>
-							<label for="q1">Q: Lorem ipsum dolor sit amet, consectetur adipisicing elit? </label> <input name="question" id="q1" type="checkbox" value="">
-							<div class="answer">
-								<p>Maecenas justo erat, consectetur vel pellentesque quis, pharetra id tellus. Morbi ut nibh nisi. Vestibulum porta sollicitudin erat et convallis. u</p>
-							</div>
-						</li>
-
-						<li>
-							<label for="q2">Q: Consectetur vel pellentesque quis? </label> <input name="question" id="q2" type="checkbox" value="">
+							<label for="q1">Q: Pulau Bawean itu, dimana ya?</label> <input name="question" id="q1" type="checkbox" value="" checked="checked">
 							<div class="big">
-								<p>Maecenas justo erat, consectetur vel pellentesque quis, pharetra id tellus. Morbi ut nibh nisi. Vestibulum porta sollicitudin erat et convallis. u</p>
-								<p>Maecenas justo erat, consectetur vel pellentesque quis, pharetra id tellus. Morbi ut nibh nisi. Vestibulum porta sollicitudin erat et convallis. u</p>
+								<p>A: Pulau Bawean merupakan salah satu pulau yang terkenal di Jawa Timur. Secara administratif pulau ini merupakan bagian dari Kabupaten Gresik. Letaknya sekitar 120 kilometer di utara pusat Kabupaten Gresik. Pulau Bawean merupakan bagian dari Kepulauan Sunda Besar.</p>
 							</div>
 						</li>
 
 						<li>
-							<label for="q3">Q: Morbi ut nibh nisi? </label> <input name="question" id="q3" type="checkbox" value="" checked="checked">
-							<div class="answer">
-								<p>Maecenas justo erat, consectetur vel pellentesque quis, pharetra id tellus. Morbi ut nibh nisi. Vestibulum porta sollicitudin erat et convallis. u</p>
+							<label for="q2">Q: Apa itu website Wonderful Bawean?</label> <input name="question" id="q2" type="checkbox" value="">
+							<div class="big">
+								<p>A: Wonderful Bawean adalah sistem informasi yang mampu menampilkan objek wisata dan memberikan informasi jika objek wisata tersebut dipilih, pengguna dapat melihat lebih detail objek wisata yang akan dicari, dan dapat mengetahui tiket masuk di pulau Bawean.</p>
 							</div>
 						</li>
 
 						<li>
-							<label for="q4">Q: Vestibulum porta sollicitudin erat? </label> <input name="question" id="q4" type="checkbox" value="">
+							<label for="q3">Q: Apakah saya bisa mendapatkan informasi tentang wisata yang ada di bawean beserta harga tiketnya?</label> <input name="question" id="q3" type="checkbox" value="">
 							<div class="answer">
-								<p>Maecenas justo erat, consectetur vel pellentesque quis, pharetra id tellus. Morbi ut nibh nisi. Vestibulum porta sollicitudin erat et convallis. u</p>
+								<p>A: Ya! Tentu saja bisa</p>
+							</div>
+						</li>
+
+						<li>
+							<label for="q4">Q: Bagaimana cara menggunakan website Wonderful Bawean dalam merencanakan liburan ke Pulau Bawean?</label> <input name="question" id="q4" type="checkbox" value="">
+							<div class="big">
+								<p>A: Cukup mudah dengan tiga langkah, yang dimulai dengan mencari di daftar wisata yang akan dikunjungi, melakukan proses pemesanan dan transaksi pembayaran, dan nikmati perjalanan anda dalam penjelajahan ini dengan menggunakan jasa yang kami berikan!</p>
 							</div>
 						</li>
 				</ul>
